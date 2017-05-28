e37b64f6edf92d64b188c33d0aee6d7515edeae9
korrai <francisavp@hotmail.com>
2017-05-27 19:11:42 -0600

    first commit

---
+++ tarea.py
@@ -0,0 +1,34 @@
+from PIL import Image
+import os
+import numpy as np
+
+imagen = Image.open("wonder.jpg")
+arr = imagen.load()
+
+#sacando size original de la imagen
+orig_A, orig_L = imagen.size 
+
+#factor de resizing
+rFac = orig_A / (orig_A / 2)
+
+#nuevos valor de width y height
+new_A, new_L = int(orig_A * rFac), int(orig_L * rFac) 
+
+#tuple de valores del size para enviar a funcion Image.new
+newSize = (new_A, new_L) 
+
+#nuevaImagen = Image.new("RGB", newSize, None)
+
+allpix = []
+
+for x in range(orig_A - 1):
+    for y in range(orig_L - 1):
+        tempixel = arr[x, y]
+        allpix.append(tempixel)
+
+arreglo = np.array(allpix)
+
+print(arreglo)
+
+#print("ARREGLO DE PIXELS:",allpix)
+
